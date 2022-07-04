691c348329b9f4c1065cc00c357ef1dcd7cba7c0
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-05 08:59:36 +0900

    Implement ftest_e

--- lib/SPVM/System.c
+++ lib/SPVM/System.c
@@ -64,9 +64,28 @@ int32_t SPVM__System__ftest_O(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__System__ftest_e(SPVM_ENV* env, SPVM_VALUE* stack) {
-
-  return env->die(env, stack, "Not yet implemented", FILE_NAME, __LINE__);
-
+  
+  void* obj_file = stack[0].oval;
+  
+  if (!obj_file) {
+    return env->die(env, stack, "A file name must be specified", FILE_NAME, __LINE__);
+  }
+  
+  const char* file = env->get_chars(env, stack, obj_file);
+  
+  int32_t result;
+  
+  struct stat st;
+  int32_t stat_result = stat(file, &st);
+  if (stat_result != 0) {
+    result = 0;
+  }
+  else {
+    result = 1;
+  }
+  
+  stack[0].ival = result;
+  
   return 0;
 }
 
