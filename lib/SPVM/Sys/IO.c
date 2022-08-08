fa8b7f81b588460273bf74891bbcb9f40d7449fc
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-08 15:37:45 +0900

    Change rmdir, unlink return value and throw exception

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -5,23 +5,23 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <errno.h>
 
 const char* FILE_NAME = "SPVM/Sys/IO.c";
 
 int32_t SPVM__Sys__IO__rmdir(SPVM_ENV* env, SPVM_VALUE* stack) {
   
-  void* obj_file = stack[0].oval;
+  void* obj_dir = stack[0].oval;
   
-  int32_t count = 0;
-  if (obj_file) {
-    const char* file = env->get_chars(env, stack, obj_file);
-    int32_t error_code = rmdir(file);
-    if (error_code == 0) {
-      count++;
-    }
+  if (!obj_dir) {
+    return env->die(env, stack, "The directory must be defined", FILE_NAME, __LINE__);
   }
   
-  stack[0].ival = count;
+  const char* dir = env->get_chars(env, stack, obj_dir);
+  int32_t status = rmdir(dir);
+  if (!(status == 0)) {
+    return env->die(env, stack, "[System Error]rmdir failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+  }
   
   return 0;
 }
@@ -29,17 +29,16 @@ int32_t SPVM__Sys__IO__rmdir(SPVM_ENV* env, SPVM_VALUE* stack) {
 int32_t SPVM__Sys__IO__unlink(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   void* obj_file = stack[0].oval;
-  
-  int32_t count = 0;
-  if (obj_file) {
-    const char* file = env->get_chars(env, stack, obj_file);
-    int32_t error_code = unlink(file);
-    if (error_code == 0) {
-      count++;
-    }
+
+  if (!obj_file) {
+    return env->die(env, stack, "The file must be defined", FILE_NAME, __LINE__);
   }
   
-  stack[0].ival = count;
+  const char* file = env->get_chars(env, stack, obj_file);
+  int32_t status = unlink(file);
+  if (!(status == 0)) {
+    return env->die(env, stack, "[System Error]unlink failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+  }
   
   return 0;
 }
