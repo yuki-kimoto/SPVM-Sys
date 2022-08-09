7fb87c8b620a9ab73805827d3e1f7a0914294d84
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-10 08:52:01 +0900

    Add Fcntl files

---
+++ lib/SPVM/Sys/Fcntl.pm
@@ -0,0 +1,45 @@
+package SPVM::Sys::Fcntl;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::Sys::Fcntl - Sys::Fcntl is a SPVM module
+
+=head1 Usage
+
+  use Sys::Fcntl;
+  
+=head1 Description
+
+C<Sys::Fcntl> is a L<SPVM> module.
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
