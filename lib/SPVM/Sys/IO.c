3c8d4bdbc22c663a9387b2607e5dd0305fa54077
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-09 12:05:44 +0900

    Add chmode

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -515,3 +515,26 @@ int32_t SPVM__Sys__IO__chdir(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   return 0;
 }
+
+int32_t SPVM__Sys__IO__chmod(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_system_class_id = SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  
+  void* obj_path = stack[0].oval;
+  
+  if (!obj_path) {
+    return env->die(env, stack, "The path must be defined", FILE_NAME, __LINE__);
+  }
+  
+  const char* path = env->get_chars(env, stack, obj_path);
+
+  int32_t mode = stack[0].ival;
+
+  int32_t status = chmod(path, mode);
+  if (status == -1) {
+    env->die(env, stack, "[System Error]chmod failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return error_system_class_id;
+  }
+  
+  return 0;
+}
