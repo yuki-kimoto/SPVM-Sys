cca8b49ab1b6f8a01ba1a5236d29a7eef1ac5758
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-16 15:19:58 +0900

    Add Sys::Timeval

---
+++ lib/SPVM/Sys/Timeval.pm
@@ -0,0 +1,45 @@
+package SPVM::Sys::Timeval;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::Sys::Timeval - Sys::Timeval is a SPVM module
+
+=head1 Usage
+
+  use Sys::Timeval;
+  
+=head1 Description
+
+C<Sys::Timeval> is a L<SPVM> module.
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
