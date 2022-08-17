8ed7cf877922dc62f84ea95302ec5adbe00124b4
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-17 09:51:01 +0900

    Add alerm

--- lib/SPVM/Sys/Process.c
+++ lib/SPVM/Sys/Process.c
@@ -1,12 +1,16 @@
 #include "spvm_native.h"
 
+#include <unistd.h>
 
-
-int32_t SPVM__Sys__Process__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Sys__Process__alarm(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   (void)stack;
   
+  int32_t seconds = stack[0].ival;
+  
+  int32_t rest_time = alarm(seconds);
+  
+  stack[0].ival = rest_time;
+  
   return 0;
 }
-
-
