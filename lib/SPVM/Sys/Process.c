e625ed92ad45fbe036a60a2fe67b9e3bc55a0be3
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-17 10:31:59 +0900

    Add kill

--- lib/SPVM/Sys/Process.c
+++ lib/SPVM/Sys/Process.c
@@ -5,6 +5,7 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <errno.h>
+#include <signal.h>
 
 const char* FILE_NAME = "Sys/Process.c";
 
@@ -86,6 +87,24 @@ int32_t SPVM__Sys__Process__sleep(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t rest_time = sleep(seconds);
   
   stack[0].ival = rest_time;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Process__kill(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  int32_t pid = stack[0].ival;
+  int32_t sig = stack[1].ival;
+  
+  int32_t status = kill(pid, sig);
+  if (status == -1) {
+    env->die(env, stack, "[System Error]kill failed:%s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = status;
   
   return 0;
 }
