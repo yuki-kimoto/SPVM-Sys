b1dc3d705a93680b26d3a7a41d0d2fef08d52e53
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-17 10:10:24 +0900

    Add getpriority setpriority

--- lib/SPVM/Sys/Process.c
+++ lib/SPVM/Sys/Process.c
@@ -1,7 +1,12 @@
 #include "spvm_native.h"
 
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+const char* FILE_NAME = "Sys/Process.c";
 
 int32_t SPVM__Sys__Process__alarm(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
@@ -25,6 +30,44 @@ int32_t SPVM__Sys__Process__fork(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t ret = fork();
   
   stack[0].ival = ret;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Process__getpriority(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+
+  int32_t which = stack[0].ival;
+  int32_t who = stack[1].ival;
+  
+  errno = 0;
+  int32_t nice = getpriority(which, who);
+  if (errno != 0) {
+    env->die(env, stack, "[System Error]getpriority failed:%s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = nice;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Process__setpriority(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  int32_t which = stack[0].ival;
+  int32_t who = stack[1].ival;
+  int32_t prio = stack[2].ival;
+  
+  int32_t status = setpriority(which, who, prio);
+  if (status == -1) {
+    env->die(env, stack, "[System Error]setpriority failed:%s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = status;
   
   return 0;
 }
