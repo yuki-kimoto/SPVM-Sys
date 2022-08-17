3209da41eddab6b2fdc85f6a4d739558cc2090d6
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-17 09:54:18 +0900

    Add fork

--- lib/SPVM/Sys/Process.c
+++ lib/SPVM/Sys/Process.c
@@ -1,5 +1,6 @@
 #include "spvm_native.h"
 
+#include <sys/types.h>
 #include <unistd.h>
 
 int32_t SPVM__Sys__Process__alarm(SPVM_ENV* env, SPVM_VALUE* stack) {
@@ -11,6 +12,19 @@ int32_t SPVM__Sys__Process__alarm(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t rest_time = alarm(seconds);
   
   stack[0].ival = rest_time;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Process__fork(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  int32_t seconds = stack[0].ival;
+  
+  int32_t ret = fork();
+  
+  stack[0].ival = ret;
   
   return 0;
 }
