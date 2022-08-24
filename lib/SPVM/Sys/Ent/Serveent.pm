36117bdb1bdca80a5eab7d7213bce2a9a8cbd0f6
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-25 07:32:00 +0900

    Add Sys::Netent, Sys::Protoent

---
+++ lib/SPVM/Sys/Ent/Serveent.pm
@@ -0,0 +1,45 @@
+package SPVM::Sys::Ent::Serveent;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::Sys::Ent::Serveent - Sys::Ent::Serveent is a SPVM module
+
+=head1 Usage
+
+  use Sys::Ent::Serveent;
+  
+=head1 Description
+
+C<Sys::Ent::Serveent> is a L<SPVM> module.
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
