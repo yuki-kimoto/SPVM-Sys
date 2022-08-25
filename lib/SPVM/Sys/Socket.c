b5bb57686e16694215a793dc598ca3b50aed4a0f
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-25 14:37:46 +0900

    Add gai_strerror

--- lib/SPVM/Sys/Socket.c
+++ lib/SPVM/Sys/Socket.c
@@ -14,11 +14,23 @@
 # define closesocket(fd) close(fd)
 #endif
 
-int32_t SPVM__Sys__Socket__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Sys__Socket__gai_strerror(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   (void)stack;
   
+  int32_t gai_error_code = stack[0].ival;
+  
+  const char* ret_gai_strerror = gai_strerror(gai_error_code);
+  
+  if (ret_gai_strerror) {
+    int32_t ret_gai_strerror_length = strlen(ret_gai_strerror);
+    
+    void* obj_gai_strerror = env->new_string(env, stack, ret_gai_strerror, ret_gai_strerror_length);
+    stack[0].oval = obj_gai_strerror;
+  }
+  else {
+    stack[0].oval = NULL;
+  }
+  
   return 0;
 }
-
-
