4be960514b7d3bcd7028a4e4a9428ee5e1b4b4e6
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-25 15:18:14 +0900

    Add getaddrinfo

--- lib/SPVM/Sys/Socket.c
+++ lib/SPVM/Sys/Socket.c
@@ -14,6 +14,58 @@
 # define closesocket(fd) close(fd)
 #endif
 
+static const char* FILE_NAME = "Sys/Socket.c";
+
+int32_t SPVM__Sys__Socket__getaddrinfo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  int32_t e = 0;
+  
+  void* obj_node = stack[0].oval;
+  
+  const char* node = NULL;
+  if (obj_node) {
+    node = env->get_chars(env, stack, obj_node);
+  }
+  
+  void* obj_service = stack[1].oval;
+  
+  const char* service = NULL;
+  if (obj_service) {
+    service = env->get_chars(env, stack, obj_service);
+  }
+  
+  void* obj_hints = stack[2].oval;
+  
+  struct addrinfo *hints = NULL;
+  if (obj_hints) {
+    hints = env->get_pointer(env, stack, obj_hints);
+  }
+  
+  void* obj_res = stack[3].oval;
+  
+  if (!obj_res) {
+    return env->die(env, stack, "The response must be defined", FILE_NAME, __LINE__);
+  }
+  
+  int32_t res_length = env->length(env, stack, obj_res);
+  
+  if (res_length == 1) {
+    return env->die(env, stack, "The length of the response must be 1", FILE_NAME, __LINE__);
+  }
+  
+  struct addrinfo *res = NULL;
+  int32_t status = getaddrinfo(node, service, hints, &res);
+  
+  if (status == 0) {
+    void* obj_res_elem = env->new_pointer_by_name(env, stack, "Sys::Addrinfo", res, &e, FILE_NAME, __LINE__);
+    if (e) { return e; }
+    env->set_elem_object(env, stack, res, 0, obj_res_elem);
+  }
+  
+  stack[0].ival = status;
+  
+  return 0;
+}
+
 int32_t SPVM__Sys__Socket__gai_strerror(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   (void)stack;
