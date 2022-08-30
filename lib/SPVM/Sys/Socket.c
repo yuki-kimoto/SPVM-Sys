9e29d3864ac80d8723674b1ad7ca2e82cb6db33e
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-30 14:33:36 +0900

    Add connect

--- lib/SPVM/Sys/Socket.c
+++ lib/SPVM/Sys/Socket.c
@@ -42,6 +42,32 @@ int32_t SPVM__Sys__Socket__socket(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+int32_t SPVM__Sys__Socket__connect(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t sockfd = stack[0].ival;
+  
+  void* obj_addr = stack[1].oval;
+  
+  if (!obj_addr) {
+    return env->die(env, stack, "The address must be defined", FILE_NAME, __LINE__);
+  }
+  
+  const struct sockaddr* addr = env->get_pointer(env, stack, obj_addr);
+  
+  int32_t addrlen = stack[2].ival;
+  
+  int32_t status = connect(sockfd, addr, addrlen);
+  
+  if (status == -1) {
+    env->die(env, stack, "[System Error]connect failed: %s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = status;
+  
+  return 0;
+}
+
 int32_t SPVM__Sys__Socket__getaddrinfo(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t e = 0;
   
@@ -99,7 +125,7 @@ int32_t SPVM__Sys__Socket__getnameinfo(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   void* obj_addr = stack[0].oval;
   
-  if (obj_addr) {
+  if (!obj_addr) {
     return env->die(env, stack, "The address must be defined", FILE_NAME, __LINE__);
   }
   
