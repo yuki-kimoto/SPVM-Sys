276ea51ebb95ff0632802b8d2826953503d49477
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-08-30 14:46:46 +0900

    Add shutdown

--- lib/SPVM/Sys/Socket.c
+++ lib/SPVM/Sys/Socket.c
@@ -112,6 +112,24 @@ int32_t SPVM__Sys__Socket__listen(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+int32_t SPVM__Sys__Socket__shutdown(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t sockfd = stack[0].ival;
+  
+  int32_t how = stack[1].ival;
+  
+  int32_t status = shutdown(sockfd, how);
+  
+  if (status == -1) {
+    env->die(env, stack, "[System Error]shutdown failed: %s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = status;
+  
+  return 0;
+}
+
 int32_t SPVM__Sys__Socket__accept(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t sockfd = stack[0].ival;
