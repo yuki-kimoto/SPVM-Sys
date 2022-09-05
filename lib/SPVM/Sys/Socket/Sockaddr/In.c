69dd0e2ca97d9459d895218c55ee37f8f4499758
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-05 14:49:43 +0900

    Implement socket address structure!

--- lib/SPVM/Sys/Socket/Sockaddr/In.c
+++ lib/SPVM/Sys/Socket/Sockaddr/In.c
@@ -1,30 +1,44 @@
 #include "spvm_native.h"
 
-const char* FILE_NAME = "Sys/IO/Stat.c";
+#ifdef _WIN32
+# include <ws2tcpip.h>
+# include <io.h>
+#else
+# include <sys/fcntl.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <netdb.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+# define closesocket(fd) close(fd)
+#endif
+
+const char* FILE_NAME = "Sys/Socket/Sockaddr/In.c";
 
 int32_t SPVM__Sys__Socket__Sockaddr__In__new(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t e = 0;
   
-  struct sockaddr_in* address = env->new_memory_stack(env, stack, sizeof(sockaddr_in));
+  struct sockaddr_in* socket_address = env->new_memory_stack(env, stack, sizeof(struct sockaddr_in));
 
-  void* obj_address = env->new_pointer_by_name(env, stack, "Sys::Socket::Sockaddr::In", address, &e, FILE_NAME, __LINE__);
+  void* obj_socket_address = env->new_pointer_by_name(env, stack, "Sys::Socket::Sockaddr::In", socket_address, &e, FILE_NAME, __LINE__);
   if (e) { return e; }
   
-  stack[0].oval = obj_address;
+  stack[0].oval = obj_socket_address;
   
   return 0;
 }
 
 int32_t SPVM__Sys__Socket__Sockaddr__In__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
   
-  void* obj_address = stack[0].oval;
+  void* obj_socket_address = stack[0].oval;
   
-  struct sockaddr_in* address = env->get_pointer(env, stack, obj_address);
+  struct sockaddr_in* socket_address = env->get_pointer(env, stack, obj_socket_address);
   
-  if (address) {
-    env->free_memory_stack(env, stack, address);
-    env->set_pointer(env, stack, obj_address, NULL);
+  if (socket_address) {
+    env->free_memory_stack(env, stack, socket_address);
+    env->set_pointer(env, stack, obj_socket_address, NULL);
   }
   
   return 0;
--- lib/SPVM/Sys/Socket/Sockaddr/Storage.c
+++ lib/SPVM/Sys/Socket/Sockaddr/Storage.c
@@ -1,12 +1,45 @@
 #include "spvm_native.h"
 
+#ifdef _WIN32
+# include <ws2tcpip.h>
+# include <io.h>
+#else
+# include <sys/fcntl.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <netdb.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+# define closesocket(fd) close(fd)
+#endif
 
+const char* FILE_NAME = "Sys/Socket/Sockaddr/Storage.c";
 
-int32_t SPVM__Sys__Socket__Sockaddr__Storage__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
+int32_t SPVM__Sys__Socket__Sockaddr__Storage__new(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t e = 0;
+  
+  struct sockaddr_strage* socket_address = env->new_memory_stack(env, stack, sizeof(struct sockaddr_strage));
+
+  void* obj_socket_address = env->new_pointer_by_name(env, stack, "Sys::Socket::Sockaddr::Storage", socket_address, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+  
+  stack[0].oval = obj_socket_address;
   
   return 0;
 }
 
-
+int32_t SPVM__Sys__Socket__Sockaddr__Storage__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_socket_address = stack[0].oval;
+  
+  struct sockaddr_strage* socket_address = env->get_pointer(env, stack, obj_socket_address);
+  
+  if (socket_address) {
+    env->free_memory_stack(env, stack, socket_address);
+    env->set_pointer(env, stack, obj_socket_address, NULL);
+  }
+  
+  return 0;
+}
