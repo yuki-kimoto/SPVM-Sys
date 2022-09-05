2b18be3589ca9e35e2254ebc101102973d3fa571
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-06 08:51:05 +0900

    Add un path

--- lib/SPVM/Sys/Socket/Sockaddr/Un.c
+++ lib/SPVM/Sys/Socket/Sockaddr/Un.c
@@ -47,7 +47,7 @@ int32_t SPVM__Sys__Socket__Sockaddr__Un__DESTROY(SPVM_ENV* env, SPVM_VALUE* stac
   return 0;
 }
 
-int32_t SPVM__Sys__Socket__Sockaddr__Un__sin_family(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Sys__Socket__Sockaddr__Un__sun_family(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   void* obj_self = stack[0].oval;
   
@@ -63,7 +63,7 @@ int32_t SPVM__Sys__Socket__Sockaddr__Un__sin_family(SPVM_ENV* env, SPVM_VALUE* s
   return 0;
 }
 
-int32_t SPVM__Sys__Socket__Sockaddr__Un__set_sin_family(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Sys__Socket__Sockaddr__Un__set_sun_family(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   void* obj_self = stack[0].oval;
   
@@ -71,6 +71,56 @@ int32_t SPVM__Sys__Socket__Sockaddr__Un__set_sin_family(SPVM_ENV* env, SPVM_VALU
   
   if (socket_address) {
     socket_address->sun_family = stack[1].ival;
+  }
+  else {
+    assert(0);
+  }
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Sockaddr__Un__sun_path(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct sockaddr_un* socket_address = env->get_pointer(env, stack, obj_self);
+  
+  if (socket_address) {
+    void* obj_path;
+    if (socket_address->sun_path) {
+      obj_path = env->new_string(env, stack, socket_address->sun_path, strlen(socket_address->sun_path));
+    }
+    else {
+      obj_path = NULL;
+    };
+    
+    stack[0].oval = obj_path;
+  }
+  else {
+    assert(0);
+  }
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Sockaddr__Un__set_sun_path(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct sockaddr_un* socket_address = env->get_pointer(env, stack, obj_self);
+  
+  if (socket_address) {
+    void* obj_path = stack[1].oval;
+    
+    const char* path = env->get_chars(env, stack, obj_path);
+    int32_t path_length = env->length(env, stack, obj_path);
+    
+    if (!(path_length <= 91)) {
+      return env->die(env, stack, "The length of the path must be less than or equal to 91", FILE_NAME, __LINE__);
+    }
+    
+    memset(socket_address->sun_path, 0, strlen(socket_address->sun_path));
+    memcpy(socket_address->sun_path, path, path_length);
   }
   else {
     assert(0);
