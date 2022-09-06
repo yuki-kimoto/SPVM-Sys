432acc810ef9a73e4e704da82f0ff418285e8f7a
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-07 07:57:58 +0900

    Move poll from Sys::Poll to Sys::IO

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -16,6 +16,7 @@
 
 #ifndef _WIN32
   #include <sys/ioctl.h>
+  #include <sys/poll.h>
 #endif
 
 
@@ -1109,6 +1110,28 @@ int32_t SPVM__Sys__IO__fcntl(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   if (status == -1) {
     env->die(env, stack, "[System Error]fcntl failed:%s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = status;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__IO__poll(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_fds = stack[0].oval;
+  
+  struct pollfd* fds = env->get_pointer(env, stack, obj_fds);
+  
+  int32_t nfds = stack[1].ival;
+  
+  int32_t timeout = stack[2].ival;
+  
+  int32_t status = poll(fds, nfds, timeout);
+  
+  if (status == -1) {
+    env->die(env, stack, "[System Error]poll failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
     return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
   }
   
