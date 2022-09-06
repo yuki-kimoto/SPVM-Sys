ca5fe9b5153b25c6fd2fed896299407956e90721
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-06 16:12:44 +0900

    Add poll constants

--- lib/SPVM/Sys/Poll/Constant.pm
+++ lib/SPVM/Sys/Poll/Constant.pm
@@ -1,12 +1,10 @@
 package SPVM::Sys::Poll::Constant;
 
-our $VERSION = '0.01';
-
 1;
 
 =head1 Name
 
-SPVM::Sys::Poll::Constant - Sys::Poll::Constant is a SPVM module
+SPVM::Sys::Poll::Constant - Poll Constant Values
 
 =head1 Usage
 
@@ -16,30 +14,138 @@ SPVM::Sys::Poll::Constant - Sys::Poll::Constant is a SPVM module
 
 C<Sys::Poll::Constant> is a L<SPVM> module.
 
-=head1 Fields
-
-
-
 =head1 Class Methods
 
+=head2 POLLERR
 
+  static method POLLERR : int ();
 
-=head1 Instance Methods
+Get the error number of C<POLLERR>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
 
+=head2 POLLHUP
 
+  static method POLLHUP : int ();
 
-=head1 Repository
+Get the error number of C<POLLHUP>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
 
+=head2 POLLIN
 
+  static method POLLIN : int ();
 
-=head1 Author
+Get the error number of C<POLLIN>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
 
-[--user-name] C<[--user-email]>
+=head2 POLLNORM
 
-=head1 Copyright & License
+  static method POLLNORM : int ();
 
-Copyright 2022-2022 [--user-name], all rights reserved.
+Get the error number of C<POLLNORM>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+=head2 POLLNVAL
+
+  static method POLLNVAL : int ();
+
+Get the error number of C<POLLNVAL>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLOUT
+
+  static method POLLOUT : int ();
+
+Get the error number of C<POLLOUT>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLPRI
+
+  static method POLLPRI : int ();
+
+Get the error number of C<POLLPRI>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLRDBAND
+
+  static method POLLRDBAND : int ();
+
+Get the error number of C<POLLRDBAND>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLRDNORM
+
+  static method POLLRDNORM : int ();
+
+Get the error number of C<POLLRDNORM>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLWRBAND
+
+  static method POLLWRBAND : int ();
+
+Get the error number of C<POLLWRBAND>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLWRNORM
+
+  static method POLLWRNORM : int ();
+
+Get the error number of C<POLLWRNORM>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+kimoto@tk2-259-39003:~/labo/modules/SPVM-Sys$ perl ~/poll_constant.pl
+=head2 POLLERR
+
+  static method POLLERR : int ();
+
+Get the constant value of C<POLLERR>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLHUP
+
+  static method POLLHUP : int ();
+
+Get the constant value of C<POLLHUP>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLIN
+
+  static method POLLIN : int ();
+
+Get the constant value of C<POLLIN>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLNORM
+
+  static method POLLNORM : int ();
+
+Get the constant value of C<POLLNORM>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLNVAL
+
+  static method POLLNVAL : int ();
+
+Get the constant value of C<POLLNVAL>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLOUT
+
+  static method POLLOUT : int ();
+
+Get the constant value of C<POLLOUT>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLPRI
+
+  static method POLLPRI : int ();
+
+Get the constant value of C<POLLPRI>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLRDBAND
+
+  static method POLLRDBAND : int ();
+
+Get the constant value of C<POLLRDBAND>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLRDNORM
+
+  static method POLLRDNORM : int ();
+
+Get the constant value of C<POLLRDNORM>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLWRBAND
+
+  static method POLLWRBAND : int ();
+
+Get the constant value of C<POLLWRBAND>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
+
+=head2 POLLWRNORM
+
+  static method POLLWRNORM : int ();
+
+Get the constant value of C<POLLWRNORM>. If the system doesn't define this constant, an exception will be thrown. The error code is set to the class id of the L<Error::NotSupported|SPVM::Error::NotSupported> class.
 
