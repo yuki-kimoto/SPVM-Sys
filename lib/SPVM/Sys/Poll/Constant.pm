820294d5202e1b246ca95b074308b04dcd4cc18b
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-06 16:02:53 +0900

    Add poll modules

---
+++ lib/SPVM/Sys/Poll/Constant.pm
@@ -0,0 +1,45 @@
+package SPVM::Sys::Poll::Constant;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::Sys::Poll::Constant - Sys::Poll::Constant is a SPVM module
+
+=head1 Usage
+
+  use Sys::Poll::Constant;
+  
+=head1 Description
+
+C<Sys::Poll::Constant> is a L<SPVM> module.
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
