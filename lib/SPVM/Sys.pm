59c7b4f1ef9344a3ebb33df35b3deb0572bbffa9
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-08 11:00:55 +0900

    Update document

--- lib/SPVM/Sys.pm
+++ lib/SPVM/Sys.pm
@@ -8,54 +8,43 @@ our $VERSION = '0.01';
 
 SPVM::Sys - System Utilities, File IO, User, Process, Socket,
 
+=head1 Usage
+
+  use Sys;
+  
+  my $is_windows = Sys->is_D_WIN32;
 
 =head1 Description
 
-C<Sys> is the system utilities file IO, user, process, socket,
+C<Sys> is the class for system utilities such as file IO, user manipulation, process, socket,
 
-=head1 Modules
+=head1 Class Methods
 
-=head2 IO
+=head2 is_D_WIN32
 
-=over
+  static method is_D_WIN32 : int ()
+
+If C<_WIN32> in C<C language> is defined, return C<1>. Otherwize return C<0>.
+
+=head1 System Modules
+
+=over 2
 
 =item * L<Sys::IO|SPVM::Sys::IO>
 
-=item * L<Sys::Sys::FiteTest|SPVM::Sys::FiteTest>
-
-=back
-
-=head2 User
-
-=over
+=item * L<Sys::FiteTest|SPVM::Sys::FiteTest>
 
 =item * L<Sys::User|SPVM::Sys::User>
 
-=head2 Socket
-
-=back
-
-=over
-
 =item * L<Sys::Socket|SPVM::Sys::Socket>
-
-=back
-
-=head2 Process
-
-=over
 
 =item * L<Sys::Process|SPVM::Sys::Process>
 
 =back
 
-=over 2
-
-=back
-
 =head1 Copyright & License
 
-Copyright Yuki Kimoto, all rights reserved.
+Copyright Yuki Kimoto 2022-2022, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
