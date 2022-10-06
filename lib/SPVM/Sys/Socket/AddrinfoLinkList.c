91af9280024bdcbc66c390ccbd0eb4d3dd7bf223
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-06 14:11:46 +0900

    Added the Sys::Socket::AddrinfoLinkList class

---
+++ lib/SPVM/Sys/Socket/AddrinfoLinkList.c
@@ -0,0 +1,14 @@
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Sys/Socket/AddrinfoLinkList.c";
+
+int32_t SPVM__Sys__Socket__AddrinfoLinkList__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
