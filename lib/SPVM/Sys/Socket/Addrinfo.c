284e6fcfc18d708904c9e2a2d5067805c265f85a
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-06 14:06:48 +0900

    Added the accessor methods to the Sys::Socket::Addrinfo class.
      [New Features]
       * Added the accessor methods to the Sys::Socket::Addrinfo class.
    
         method ai_flags : int ();
    
         method set_ai_flags : void ($ai_flags : int);
    
         method ai_family : int ();
    
         method set_ai_family : void ($ai_family : int);
    
         method ai_socktype : int ();
    
         method set_ai_socktype : void ($ai_socktype : int);
    
         method ai_protocol : int ();
    
         method set_ai_protocol : void ($ai_protocol : int);
    
         method copy_ai_addr : Sys::Socket::Sockaddr ();
    
         method copy_ai_canonname : string ();

--- lib/SPVM/Sys/Socket/Addrinfo.c
+++ lib/SPVM/Sys/Socket/Addrinfo.c
@@ -60,3 +60,160 @@ int32_t SPVM__Sys__Socket__Addrinfo__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+int32_t SPVM__Sys__Socket__Addrinfo__ai_flags(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  stack[0].ival = st_addrinfo->ai_flags;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__set_ai_flags(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  st_addrinfo->ai_flags = stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__ai_family(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  stack[0].ival = st_addrinfo->ai_family;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__set_ai_family(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  st_addrinfo->ai_family = stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__ai_socktype(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  stack[0].ival = st_addrinfo->ai_socktype;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__set_ai_socktype(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  st_addrinfo->ai_socktype = stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__ai_protocol(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  stack[0].ival = st_addrinfo->ai_protocol;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__set_ai_protocol(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  st_addrinfo->ai_protocol = stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__ai_addrlen(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  stack[0].ival = (int32_t)st_addrinfo->ai_addrlen;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__set_ai_addrlen(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  st_addrinfo->ai_addrlen = (socklen_t)stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__copy_ai_addr(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t e = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  struct sockaddr* ai_addr = st_addrinfo->ai_addr;
+  
+  void* obj_ai_addr_clone = NULL;
+  if (ai_addr) {
+    void* obj_ai_addr = env->new_pointer_by_name(env, stack, "Sys::Socket::Sockaddr", ai_addr, &e, FILE_NAME, __LINE__);
+    if (e) { return e; }
+    
+    // Calls the clone method.
+    {
+      stack[0].oval = obj_ai_addr;
+      int32_t args_stack_length = 1;
+      e = env->call_instance_method_by_name(env, stack, obj_ai_addr, "clone", args_stack_length, FILE_NAME, __LINE__);
+      if (e) { return e; };
+      obj_ai_addr_clone = stack[0].oval;
+    }
+  }
+  
+  stack[0].oval = obj_ai_addr_clone;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Socket__Addrinfo__copy_ai_canonname(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_self = stack[0].oval;
+  
+  struct addrinfo* st_addrinfo = env->get_pointer(env, stack, obj_self);
+  
+  char* ai_canonname = st_addrinfo->ai_canonname;
+  
+  void* obj_ai_canonname = NULL;
+  if (ai_canonname) {
+    obj_ai_canonname = env->new_string(env, stack, ai_canonname, strlen(ai_canonname));
+  }
+  
+  stack[0].oval = obj_ai_canonname;
+  
+  return 0;
+}
