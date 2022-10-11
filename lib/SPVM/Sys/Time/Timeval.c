37c51dc5dc55448dd41ecd7932bf97664789dffa
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-12 08:06:43 +0900

    Added the templates of Sys::Time, Sys::Time::Tm, Sys::Time::Timeval

---
+++ lib/SPVM/Sys/Time/Timeval.c
@@ -0,0 +1,14 @@
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Sys/Time/Timeval.c";
+
+int32_t SPVM__Sys__Time__Timeval__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
