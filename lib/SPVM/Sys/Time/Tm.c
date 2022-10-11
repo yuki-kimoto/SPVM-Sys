1c35958639c535c6f570ce0b93acb2aa6cf80351
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-12 08:13:12 +0900

    Implement Sys::Time::Tm and update MANIFEST and update Changes

--- lib/SPVM/Sys/Time/Tm.c
+++ lib/SPVM/Sys/Time/Tm.c
@@ -1,14 +1,245 @@
 #include "spvm_native.h"
 
+#include <time.h>
 
+static const char* FILE_NAME = "Time/Tm.c";
 
-static const char* FILE_NAME = "Sys/Time/Tm.c";
+int32_t SPVM__Sys__Time__Tm__new(SPVM_ENV* env, SPVM_VALUE* stack) {
 
-int32_t SPVM__Sys__Time__Tm__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
+  int32_t e;
+  
+  struct tm* st_tm = env->alloc_memory_block_zero(env, sizeof(struct tm));
+  
+  void* obj_tm = env->new_pointer_by_name(env, stack, "Time::Tm", st_tm, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+
+  stack[0].oval = obj_tm;
   
   return 0;
 }
 
+int32_t SPVM__Sys__Time__Tm__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  if (obj_tm != NULL) {
+    struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+    env->free_memory_block(env, st_tm);
+  }
+  
+  return 0;
+}
 
+int32_t SPVM__Sys__Time__Tm__tm_sec(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_sec;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_min(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_min;
+  
+  return 0;
+}
+int32_t SPVM__Sys__Time__Tm__tm_hour(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_hour;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_mday(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_mday;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_mon(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_mon;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_year(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_year;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_wday(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_wday;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_yday(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_yday;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__tm_isdst(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  stack[0].ival = st_tm->tm_isdst;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_sec(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t sec = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_sec = sec;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_min(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t min = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_min = min;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_hour(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t hour = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_hour = hour;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_mday(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t mday = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_mday = mday;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_mon(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t mon = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_mon = mon;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_year(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t year = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_year = year;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_wday(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t wday = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_wday = wday;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_yday(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t yday = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_yday = yday;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Time__Tm__set_tm_isdst(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* obj_tm = stack[0].oval;
+  
+  int32_t isdst = stack[1].ival;
+  
+  struct tm* st_tm = env->get_pointer(env, stack, obj_tm);
+  
+  st_tm->tm_isdst = isdst;
+  
+  return 0;
+}
--- lib/SPVM/Sys/Time/Tm.pm
+++ lib/SPVM/Sys/Time/Tm.pm
@@ -1,45 +1,210 @@
 package SPVM::Sys::Time::Tm;
 
-our $VERSION = '0.01';
-
 1;
 
 =head1 Name
 
-SPVM::Sys::Time::Tm - Sys::Time::Tm is a SPVM module
+SPVM::Sys::Time::Tm - struct tm in C language
 
 =head1 Usage
-
+  
   use Sys::Time::Tm;
   
+  # Time information
+  my $tm = Sys::Time::Tm->new;
+  
+  # Set and get second
+  my $sec = $tm->tm_sec;
+  $tm->set_tm_sec(12);
+  
+  # Set and get minutes
+  my $min = $tm->tm_min;
+  $tm->set_tm_min(34);
+  
+  # Set and get hour
+  my $hour = $tm->tm_hour;
+  $tm->set_tm_hour(12);
+
+  # Set and get day of month
+  my $mday = $tm->tm_mday;
+  $tm->set_tm_mday(4);
+
+  # Set and get month
+  my $mon = $tm->tm_mon;
+  $tm->set_tm_mon(3);
+
+  # Set and get year
+  my $year = $tm->tm_year;
+  $tm->set_tm_year(1);
+
+  # Set and get week day
+  my $wday = $tm->tm_wday;
+  $tm->set_tm_wday(12);
+
+  # Set and get day of year
+  my $yday = $tm->tm_yday;
+  $tm->set_tm_yday(234);
+
+  # Set and get the specified time occurs during Daylight Saving Time
+  my $isdst = $tm->tm_isdst;
+  $tm->set_tm_isdst(1);
+
 =head1 Description
 
-C<Sys::Time::Tm> is a L<SPVM> module.
-
-=head1 Fields
-
-
+C<Sys::Time::Tm> is the time information that date structure is C<struct tm> of C<C language>.
 
 =head1 Class Methods
 
+=head2 new
 
+  static method new : Sys::Time::Tm ()
+
+Create a new L<Sys::Time::Tm|SPVM::Sys::Time::Tm> object.
+
+  my $tm = Sys::Time::Tm->new;
 
 =head1 Instance Methods
 
+=head2 sec
 
+  method sec : int ()
 
-=head1 Repository
+Get second. This is same as getting C<tm_sec> of C<struct tm>.
 
+  my $sec = $tm->tm_sec;
 
+=head2 set_tm_sec
 
-=head1 Author
+  method set_tm_sec : void ($sec : int)
 
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+Set second. This is same as setting C<tm_sec> of C<struct tm>.
 
-=head1 Copyright & License
+  $tm->set_tm_sec(12);
 
-Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+=head2 min
+  
+  method min : int ()
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+Get minutes. This is same as getting C<tm_min> of C<struct tm>.
 
+  my $min = $tm->tm_min;
+
+=head2 set_tm_min
+
+  method set_tm_min : void ($min : int)
+
+Set minutes. This is same as setting C<tm_min> of C<struct tm>.
+
+  $tm->set_tm_min(34);
+
+=head2 hour
+
+  method hour : int ()
+
+Get hour. This is same as getting C<tm_hour> of C<struct tm>.
+
+  my $hour = $tm->tm_hour;
+
+=head2 set_tm_hour
+
+  method set_tm_hour : void ($hour : int)
+
+Set hour. This is same as setting C<tm_hour> of C<struct tm>.
+
+  $tm->set_tm_hour(12);
+
+=head2 mday
+
+  method mday : int ()
+
+Get day of month. This is same as getting C<tm_mday> of C<struct tm>.
+
+  my $mday = $tm->tm_mday;
+
+=head2 set_tm_mday
+
+  method set_tm_mday : void ($mday : int)
+
+Set day of month. This is same as setting C<tm_mday> of C<struct tm>.
+
+  $tm->set_tm_mday(4);
+
+=head2 mon
+
+  method mon : int ()
+
+Get month. This is same as getting C<tm_mon> of C<struct tm>.
+
+  my $mon = $tm->tm_mon;
+
+=head2 set_tm_mon
+
+  method set_tm_mon : void ($mon : int)
+
+Set month. This is same as setting C<tm_mon> of C<struct tm>.
+
+  $tm->set_tm_mon(3);
+
+=head2 year
+
+  method year : int ()
+
+Get year. This is same as getting C<tm_year> of C<struct tm>.
+
+  my $year = $tm->tm_year;
+
+=head2 set_tm_year
+
+  method set_tm_year : void ($year : int)
+
+Set year. This is same as setting C<tm_year> of C<struct tm>.
+
+  $tm->set_tm_year(1);
+
+=head2 wday
+
+  method wday : int ()
+
+Get weekday. This is same as getting C<tm_wday> of C<struct tm>.
+
+  my $wday = $tm->tm_wday;
+
+=head2 set_tm_wday
+
+  method set_tm_wday : void ($wday : int)
+
+Set weekday. This is same as setting C<tm_wday> of C<struct tm>.
+
+  $tm->set_tm_wday(12);
+
+=head2 yday
+
+  method yday : int ()
+
+Get day of year. This is same as getting C<tm_yday> of C<struct tm>.
+
+  my $yday = $tm->tm_yday;
+
+=head2 set_tm_yday
+
+  method set_tm_wday : void ($wday : int)
+
+Set day of year. This is same as setting C<tm_yday> of C<struct tm>.
+
+  $tm->set_tm_yday(234);
+
+=head2 isdst
+
+  method isdst : int ()
+
+Get the flag whether the time is daylight saving time. This is same as getting C<tm_yday> of C<struct tm>.
+
+  my $isdst = $tm->tm_isdst;
+
+=head2 set_tm_isdst
+
+  method set_tm_isdst : void ($isdst : int)
+
+Set the flag whether the time is daylight saving time.  This is same as getting C<tm_isdst> of C<struct tm>.
+
+  $tm->set_tm_isdst(1);
