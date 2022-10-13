92e7dcfa78c0b35de01f37498833912f9c84b419
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-13 09:23:50 +0900

    Added the template of the Sys::Time::Itimerval

---
+++ lib/SPVM/Sys/Time/Itimerval.c
@@ -0,0 +1,14 @@
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Sys/Time/Itimerval.c";
+
+int32_t SPVM__Sys__Time__Itimerval__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
