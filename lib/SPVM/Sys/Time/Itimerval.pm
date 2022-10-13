30ffe5f1195c05f7b6984c9d08f57201fb9696e3
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-13 09:48:21 +0900

    Implement the Sys::Time::Itimerval class.
      [New Features]
        * Added the Sys::Time::Itimerval class.

--- lib/SPVM/Sys/Time/Itimerval.pm
+++ lib/SPVM/Sys/Time/Itimerval.pm
@@ -1,45 +1,69 @@
 package SPVM::Sys::Time::Itimerval;
 
-our $VERSION = '0.01';
-
 1;
 
 =head1 Name
 
-SPVM::Sys::Time::Itimerval - Sys::Time::Itimerval is a SPVM module
+SPVM::Sys::Time::Itimerval - struct timeval in C language
 
 =head1 Usage
-
+  
   use Sys::Time::Itimerval;
   
+  my $tv = Sys::Time::Itimerval->new;
+  
+  my $it_interval = $tv->copy_it_interval;
+  $tv->set_it_interval(12);
+  
+  my $it_value = $tv->copy_it_value;
+  $tv->set_it_value(34);
+
 =head1 Description
 
-C<Sys::Time::Itimerval> is a L<SPVM> module.
+C<Sys::Time::Itimerval> represents C<struct timeval> in C<C language>.
 
-=head1 Fields
-
-
+See L<gettimeofday(2) - Linux man page|https://linux.die.net/man/2/gettimeofday> about C<struct timeval> in Linux.
 
 =head1 Class Methods
 
+=head2 new
 
+  static method new : Sys::Time::Itimerval ()
+
+Creates a new C<Sys::Time::Itimerval> object.
+
+  my $tv = Sys::Time::Itimerval->new;
 
 =head1 Instance Methods
 
+=head2 copy_it_interval
 
+  method copy_it_interval : Sys::Time::Timeval ()
 
-=head1 Repository
+Copies C<it_interval>.
 
+  my $it_interval = $tv->copy_it_interval;
 
+=head2 set_it_interval
 
-=head1 Author
+  method set_it_interval : void ($it_interval : Sys::Time::Timeval)
 
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+Sets C<it_interval>.
 
-=head1 Copyright & License
+  $tv->set_it_interval(12);
 
-Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+=head2 copy_it_value
+  
+  method copy_it_value : Sys::Time::Timeval ()
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+Copies C<it_value>.
 
+  my $it_value = $tv->copy_it_value;
+
+=head2 set_it_value
+
+  method set_it_value : void ($it_value : Sys::Time::Timeval)
+
+Sets C<it_value>.
+
+  $tv->set_it_value(34);
