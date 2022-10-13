81ac737a8d91be8ff435e5be9de3db4a929ba879
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-13 13:54:05 +0900

    Removed remained timersub

--- lib/SPVM/Sys/Time.c
+++ lib/SPVM/Sys/Time.c
@@ -35,38 +35,3 @@ int32_t SPVM__Sys__Time__gettimeofday(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
-int32_t SPVM__Sys__Time__timersub(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-  
-  void* obj_a = stack[0].oval;
-  struct timeval* st_a = NULL;
-  if (obj_a) {
-    st_a = env->get_pointer(env, stack, obj_a);
-  }
-  else {
-    return env->die(env, stack, "The a must be defined", FILE_NAME, __LINE__);
-  }
-
-  void* obj_b = stack[1].oval;
-  struct timeval* st_b = NULL;
-  if (obj_b) {
-    st_b = env->get_pointer(env, stack, obj_b);
-  }
-  else {
-    return env->die(env, stack, "The b must be defined", FILE_NAME, __LINE__);
-  }
-  
-  void* obj_res = stack[2].oval;
-  struct timeval* st_res = NULL;
-  if (obj_res) {
-    st_res = env->get_pointer(env, stack, obj_res);
-  }
-  else {
-    return env->die(env, stack, "The res must be defined", FILE_NAME, __LINE__);
-  }
-  
-  timersub(st_a, st_b, st_res);
-  
-  return 0;
-}
