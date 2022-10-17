fe64df5eda562d40fbf67cb6fa4e7f6a1cf09a25
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-18 08:53:11 +0900

    Fixed select error message

--- lib/SPVM/Sys/Select.c
+++ lib/SPVM/Sys/Select.c
@@ -10,6 +10,53 @@
 
 const char* FILE_NAME = "Sys/Select.c";
 
+// static functions are copied from Sys/Select.c
+static int32_t socket_errno (void) {
+#ifdef _WIN32
+  return WSAGetLastError();
+#else
+  return errno;
+#endif
+}
+
+#ifdef _WIN32
+static void* socket_strerror_string_win (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
+  char* error_message = NULL;
+  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, 
+                 NULL, error_number,
+                 MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+                 (LPSTR)&error_message, length, NULL);
+  
+  void* obj_error_message = env->new_string(env, stack, error_message, strlen(error_message));
+  
+  LocalFree(error_message);
+  
+  return obj_error_message;
+}
+#endif
+
+static void* socket_strerror_string (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
+  void*
+#ifdef _WIN32
+  obj_strerror_value = socket_strerror_string_win(env, stack, error_number, length);
+#else
+  obj_strerror_value = env->strerror_string(env, stack, error_number, length);
+#endif
+  return obj_strerror_value;
+}
+
+
+static const char* socket_strerror(SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
+  void* obj_socket_strerror = socket_strerror_string(env, stack, error_number, length);
+  
+  const char* ret_socket_strerror = NULL;
+  if (obj_socket_strerror) {
+    ret_socket_strerror = env->get_chars(env, stack, obj_socket_strerror);
+  }
+  
+  return ret_socket_strerror;
+}
+
 int32_t SPVM__Sys__Select__select(SPVM_ENV* env, SPVM_VALUE* stack) {
 
   int32_t fd = stack[0].ival;
@@ -47,7 +94,7 @@ int32_t SPVM__Sys__Select__select(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t updated_fds_count = select(fd, readfds, writefds, exceptfds, timeout);
   
   if (updated_fds_count == -1) {
-    env->die(env, stack, "[System Error]select failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    env->die(env, stack, "[System Error]select failed: %s", socket_strerror(env, stack, socket_errno(), 0), FILE_NAME, __LINE__);
     return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
   }
 
