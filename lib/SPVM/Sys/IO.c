a8e12d9287c274d12f3fbaf06dd9741776e08786
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-17 16:57:33 +0900

    Added the following method to the Sys::IO class.
      [New Features]
        * Added the following method to the Sys::IO class.
          static method select : int ($nfds : int, $readfds : Sys::IO::Fd_set, $writefds : Sys::IO::Fd_set, $exceptfds : Sys::IO::Fd_set, $timeout : Sys::Time::Timeval);

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -1644,7 +1644,7 @@ int32_t SPVM__Sys__IO__FD_CLR(SPVM_ENV* env, SPVM_VALUE* stack) {
   }
 
   if (!(fd < FD_SETSIZE)) {
-    return env->die(env, stack, "The $fd must be less than FD_CLRSIZE", FILE_NAME, __LINE__);
+    return env->die(env, stack, "The $fd must be less than FD_SETSIZE", FILE_NAME, __LINE__);
   }
   
   void* obj_set = stack[1].oval;
@@ -1669,7 +1669,7 @@ int32_t SPVM__Sys__IO__FD_ISSET(SPVM_ENV* env, SPVM_VALUE* stack) {
   }
 
   if (!(fd < FD_SETSIZE)) {
-    return env->die(env, stack, "The $fd must be less than FD_ISSETSIZE", FILE_NAME, __LINE__);
+    return env->die(env, stack, "The $fd must be less than FD_SETSIZE", FILE_NAME, __LINE__);
   }
   
   void* obj_set = stack[1].oval;
@@ -1686,3 +1686,50 @@ int32_t SPVM__Sys__IO__FD_ISSET(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   return 0;
 }
+
+int32_t SPVM__Sys__IO__select(SPVM_ENV* env, SPVM_VALUE* stack) {
+
+  int32_t fd = stack[0].ival;
+  if (!(fd >= 0)) {
+    return env->die(env, stack, "The $fd must be greater than or equal to 0", FILE_NAME, __LINE__);
+  }
+  if (!(fd < FD_SETSIZE)) {
+    return env->die(env, stack, "The $fd must be less than FD_SETSIZE", FILE_NAME, __LINE__);
+  }
+  
+  void* obj_readfds = stack[1].oval;
+  fd_set* readfds = NULL;
+  if (obj_readfds) {
+    readfds = env->get_pointer(env, stack, obj_readfds);
+  }
+  
+  void* obj_writefds = stack[2].oval;
+  fd_set* writefds = NULL;
+  if (obj_writefds) {
+    writefds = env->get_pointer(env, stack, obj_writefds);
+  }
+  
+  void* obj_exceptfds = stack[3].oval;
+  fd_set* exceptfds = NULL;
+  if (obj_exceptfds) {
+    exceptfds = env->get_pointer(env, stack, obj_exceptfds);
+  }
+  
+  void* obj_timeout = stack[4].oval;
+  struct timeval* timeout = NULL;
+  if (obj_timeout) {
+    timeout = env->get_pointer(env, stack, obj_timeout);
+  }
+  
+  int32_t updated_fds_count = select(fd, readfds, writefds, exceptfds, timeout);
+  
+  if (updated_fds_count == -1) {
+    env->die(env, stack, "[System Error]select failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+
+  stack[0].ival = updated_fds_count;
+  
+  return 0;
+}
+
