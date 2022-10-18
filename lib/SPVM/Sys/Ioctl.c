ca2bf3607bd3f7c553003a18f2f371bf65d97043
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-18 11:32:54 +0900

    ioctl throws an exception

--- lib/SPVM/Sys/Ioctl.c
+++ lib/SPVM/Sys/Ioctl.c
@@ -6,8 +6,56 @@
 #  include <sys/ioctl.h>
 #endif
 
+#include <errno.h>
 
 const char* FILE_NAME = "Sys/Ioctl.c";
+
+// static functions are copied from Sys/Socket.c
+static int32_t socket_errno (void) {
+#ifdef _WIN32
+  return WSAGetLastError();
+#else
+  return errno;
+#endif
+}
+
+#ifdef _WIN32
+static void* socket_strerror_string_win (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
+  char* error_message = NULL;
+  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, 
+                 NULL, error_number,
+                 MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+                 (LPSTR)&error_message, length, NULL);
+  
+  void* obj_error_message = env->new_string(env, stack, error_message, strlen(error_message));
+  
+  LocalFree(error_message);
+  
+  return obj_error_message;
+}
+#endif
+
+static void* socket_strerror_string (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
+  void*
+#ifdef _WIN32
+  obj_strerror_value = socket_strerror_string_win(env, stack, error_number, length);
+#else
+  obj_strerror_value = env->strerror_string(env, stack, error_number, length);
+#endif
+  return obj_strerror_value;
+}
+
+
+static const char* socket_strerror(SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
+  void* obj_socket_strerror = socket_strerror_string(env, stack, error_number, length);
+  
+  const char* ret_socket_strerror = NULL;
+  if (obj_socket_strerror) {
+    ret_socket_strerror = env->get_chars(env, stack, obj_socket_strerror);
+  }
+  
+  return ret_socket_strerror;
+}
 
 int32_t SPVM__Sys__Ioctl__ioctl(SPVM_ENV* env, SPVM_VALUE* stack) {
 
@@ -144,6 +192,11 @@ int32_t SPVM__Sys__Ioctl__ioctl(SPVM_ENV* env, SPVM_VALUE* stack) {
   }
 
 #endif
+
+  if (ret == -1) {
+    env->die(env, stack, "[System Error]ioctl failed: %s", socket_strerror(env, stack, socket_errno(), 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
 
   stack[0].ival = ret;
   
