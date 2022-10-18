7a08d89e17472fba2257ecc04a4317e1bfe0a812
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-18 10:15:38 +0900

    Remove poll.h and poll function from Sys/IO.c

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -17,7 +17,6 @@
 #ifdef _WIN32
   #include <direct.h>
 #else
-  #include <poll.h>
   #include <sys/ioctl.h>
 #endif
 
@@ -1567,33 +1566,6 @@ int32_t SPVM__Sys__IO__ioctl(SPVM_ENV* env, SPVM_VALUE* stack) {
   }
   
   stack[0].ival = ret;
-  
-  return 0;
-#endif
-}
-
-int32_t SPVM__Sys__IO__poll(SPVM_ENV* env, SPVM_VALUE* stack) {
-#ifdef _WIN32
-  env->die(env, stack, "The \"poll\" method in the class \"Sys::IO\" is not supported on this system", FILE_NAME, __LINE__);
-  return SPVM_NATIVE_C_CLASS_ID_ERROR_NOT_SUPPORTED;
-#else
-  
-  void* obj_fds = stack[0].oval;
-  
-  struct pollfd* fds = env->get_pointer(env, stack, obj_fds);
-  
-  int32_t nfds = stack[1].ival;
-  
-  int32_t timeout = stack[2].ival;
-  
-  int32_t ready_count = poll(fds, nfds, timeout);
-  
-  if (ready_count == -1) {
-    env->die(env, stack, "[System Error]poll failed:%s.", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
-    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
-  }
-  
-  stack[0].ival = ready_count;
   
   return 0;
 #endif
