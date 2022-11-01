25cc139d257eaf00f6e7c4140a2d863c535aab5a
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-11-02 08:16:47 +0900

    Copy the kill, alarm, ualarm method from the Sys::Process class to the Sys::Signal class

--- lib/SPVM/Sys/Signal.c
+++ lib/SPVM/Sys/Signal.c
@@ -1,14 +1,72 @@
 #include "spvm_native.h"
 
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <assert.h>
 
 static const char* FILE_NAME = "Sys/Signal.c";
 
-int32_t SPVM__Sys__Signal__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Sys__Signal__kill(SPVM_ENV* env, SPVM_VALUE* stack) {
+#ifdef _WIN32
+  env->die(env, stack, "kill is not supported on this system", FILE_NAME, __LINE__);
+  return SPVM_NATIVE_C_CLASS_ID_ERROR_NOT_SUPPORTED;
+#else
   (void)env;
   (void)stack;
   
+  int32_t pid = stack[0].ival;
+  int32_t sig = stack[1].ival;
+  
+  int32_t status = kill(pid, sig);
+  if (status == -1) {
+    env->die(env, stack, "[System Error]kill failed:%s", env->strerror(env, stack, errno, 0), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
+  
+  stack[0].ival = status;
+  
   return 0;
+#endif
 }
 
+int32_t SPVM__Sys__Signal__alarm(SPVM_ENV* env, SPVM_VALUE* stack) {
+#ifdef _WIN32
+  env->die(env, stack, "alarm is not supported on this system", FILE_NAME, __LINE__);
+  return SPVM_NATIVE_C_CLASS_ID_ERROR_NOT_SUPPORTED;
+#else
+  (void)env;
+  (void)stack;
+  
+  int32_t seconds = stack[0].ival;
+  
+  int32_t rest_time = alarm(seconds);
+  
+  stack[0].ival = rest_time;
+  
+  return 0;
+#endif
+}
+
+int32_t SPVM__Sys__Signal__ualarm(SPVM_ENV* env, SPVM_VALUE* stack) {
+#ifdef _WIN32
+  env->die(env, stack, "ualarm is not supported on this system", FILE_NAME, __LINE__);
+  return SPVM_NATIVE_C_CLASS_ID_ERROR_NOT_SUPPORTED;
+#else
+  (void)env;
+  (void)stack;
+  
+  int64_t usecs = stack[0].lval;
+
+  int64_t interval = stack[1].lval;
+  
+  int32_t rest_usecs = ualarm(usecs, interval);
+  
+  stack[0].lval = rest_usecs;
+  
+  return 0;
+#endif
+}
 
