7404c848f9e9598903ff77c29d54066f18d106db
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-11-02 07:53:43 +0900

    Added the Sys::Signal and Sys::Signal::Constant class.
      [New Features]
        * Added the Sys::Signal and Sys::Signal::Constant class.

---
+++ lib/SPVM/Sys/Signal/Constant.c
@@ -0,0 +1,14 @@
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Sys/Signal/Constant.c";
+
+int32_t SPVM__Sys__Signal__Constant__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
