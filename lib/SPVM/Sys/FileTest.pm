ee2996214c0d42d177d67673680e3a15acbf1e6a
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-03 12:31:13 +0900

    Reorder POD

--- lib/SPVM/Sys/FileTest.pm
+++ lib/SPVM/Sys/FileTest.pm
@@ -29,11 +29,41 @@ C<Sys::FileTest> is the class for file tests.
 
 =head1 Class Methods
 
+=head2 A
+
+  static method A : double ($file : string, $base_time : long)
+
+Script start time(base time) minus file access time, in days.
+
+=head2 C
+
+  static method C : double ($file : string, $base_time : long)
+
+Script start time(base time) minus file inode change time, in days.
+
+=head2 M
+
+  static method M : double ($file : string, $base_time : long)
+
+Script start time(base time) minus file modification time, in days.
+
+=head2 O
+
+  static method O : int ($file : string)
+
+File is owned by real uid.
+
 =head2 R
 
   static method R : int ($file : string)
 
 File is readable by real uid/gid.
+
+=head2 S
+
+  static method S : int ($file : string)
+
+File is a socket.
 
 =head2 W
 
@@ -47,95 +77,17 @@ File is writable by real uid/gid.
 
 File is executable by real uid/gid.
 
-=head2 r
-
-  static method r : int ($file : string)
-
-File is readable by effective uid/gid.
-
-=head2 w
-
-  static method w : int ($file : string)
-
-File is writable by effective uid/gid.
-
-=head2 x
-
-  static method x : int ($file : string)
-
-File is executable by effective uid/gid.
-
-=head2 e
-
-  static method e : int ($file : string)
-
-File exists.
-
-=head2 s
-
-  static method s : long ($file : string)
-
-File has nonzero size (returns size in bytes).
-
-=head2 M
-
-  static method M : double ($file : string, $base_time : long)
-
-Script start time(base time) minus file modification time, in days.
-
-=head2 C
-
-  static method C : double ($file : string, $base_time : long)
-
-Script start time(base time) minus file inode change time, in days.
-
-=head2 A
-
-  static method A : double ($file : string, $base_time : long)
-
-Script start time(base time) minus file access time, in days.
-
-=head2 O
-
-  static method O : int ($file : string)
-
-File is owned by real uid.
-
-=head2 o
-
-  static method o : int ($file : string)
-
-File is owned by effective uid.
-
-=head2 z
-
-  static method z : int ($file : string)
-
-File has zero size (is empty).
-
-=head2 S
-
-  static method S : int ($file : string)
-
-File is a socket.
-
-=head2 c
-
-  static method c : int ($file : string)
-
-File is a character special file.
- 
 =head2 b
 
   static method b : int ($file : string)
 
 File is a block special file.
 
-=head2 f
+=head2 c
 
-  static method f : int ($file : string)
+  static method c : int ($file : string)
 
-File is a plain file.
+File is a character special file.
 
 =head2 d
 
@@ -143,17 +95,17 @@ File is a plain file.
 
 File is a directory.
 
-=head2 p
+=head2 e
 
-  static method p : int ($file : string)
+  static method e : int ($file : string)
 
-File is a named pipe (FIFO), or Filehandle is a pipe.
- 
-=head2 u
+File exists.
 
-  static method u : int ($file : string)
+=head2 f
 
-File has setuid bit set.
+  static method f : int ($file : string)
+
+File is a plain file.
 
 =head2 g
 
@@ -172,4 +124,51 @@ File has sticky bit set.
   static method l : int ($file : string)
 
 File is a symbolic link (false if symlinks aren't supported by the file system).
-                    
+
+=head2 o
+
+  static method o : int ($file : string)
+
+File is owned by effective uid.
+
+=head2 p
+
+  static method p : int ($file : string)
+
+File is a named pipe (FIFO), or Filehandle is a pipe.
+
+=head2 r
+
+  static method r : int ($file : string)
+
+File is readable by effective uid/gid.
+
+=head2 s
+
+  static method s : long ($file : string)
+
+File has nonzero size (returns size in bytes).
+
+=head2 u
+
+  static method u : int ($file : string)
+
+File has setuid bit set.
+
+=head2 w
+
+  static method w : int ($file : string)
+
+File is writable by effective uid/gid.
+
+=head2 x
+
+  static method x : int ($file : string)
+
+File is executable by effective uid/gid.
+
+=head2 z
+
+  static method z : int ($file : string)
+
+File has zero size (is empty).
