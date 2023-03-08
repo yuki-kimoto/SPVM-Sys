22557f1add9f49efe4479524bcaab93fce1ace91
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-03-09 08:19:20 +0900

    Added the template of Sys::IO::Windows

---
+++ lib/SPVM/Sys/IO/Windows.pm
@@ -0,0 +1,45 @@
+package SPVM::Sys::IO::Windows;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Sys::IO::Windows - Short Description
+
+=head1 Description
+
+C<SPVM::Sys::IO::Windows> is the C<Sys::IO::Windows> class in L<SPVM> language.
+
+=head1 Usage
+
+  use Sys::IO::Windows;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2023-2023 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
