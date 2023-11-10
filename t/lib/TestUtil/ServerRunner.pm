c645b9b612d81bba7cbd255ddf801ca8702ea0eb
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-11 07:59:24 +0900

    Some methods in ServerRunner becomes class methods.

--- t/lib/TestUtil/ServerRunner.pm
+++ t/lib/TestUtil/ServerRunner.pm
@@ -13,8 +13,41 @@ my $localhost = "127.0.0.1";
 # Fields
 sub port { shift->{port} }
 
-# Functions
+# Class Methods
+sub new {
+  my $class = shift;
+  
+  my $self = {
+    @_
+  };
+  
+  bless $self, ref $class || $class;
+  
+  my $port = $self->search_available_port;
+  
+  $self->{port} = $port;
+  
+  my $code = $self->{code};
+  
+  my $process_id = fork;
+  
+  # Child
+  if ($process_id == 0) {
+    $code->($port);
+    exit;
+  }
+  else {
+    $self->{process_id} = $process_id;
+    
+    $self->wait_port_prepared($port);
+  }
+  
+  return $self;
+}
+
 sub search_available_port {
+  my ($class) = @_;
+  
   my $try_port = 20000;
   
   my $available_port;
@@ -48,7 +81,7 @@ sub search_available_port {
 }
 
 sub wait_port_prepared {
-  my ($port) = @_;
+  my ($class, $port) = @_;
   
   my $max_wait = 3;
   my $wait_time = 0.1;
@@ -75,7 +108,7 @@ sub wait_port_prepared {
 }
 
 sub kill_term_and_wait {
-  my ($process_id) = @_;
+  my ($class, $process_id) = @_;
   
   kill 'TERM', $process_id;
   
@@ -85,43 +118,11 @@ sub kill_term_and_wait {
   }
 }
 
-# Class Methods
-sub new {
-  my $class = shift;
-  
-  my $self = {
-    @_
-  };
-  
-  bless $self, ref $class || $class;
-  
-  my $port = &search_available_port;
-  
-  $self->{port} = $port;
-  
-  my $code = $self->{code};
-  
-  my $process_id = fork;
-  
-  # Child
-  if ($process_id == 0) {
-    $code->($port);
-    exit;
-  }
-  else {
-    $self->{process_id} = $process_id;
-    
-    &wait_port_prepared($port);
-  }
-  
-  return $self;
-}
-
 # Instance Methods
 sub DESTROY {
   my $self = shift;
   
   my $process_id = $self->{process_id};
   
-  TestUtil::Socket::kill_term_and_wait($process_id);
+  $self->kill_term_and_wait($process_id);
 }
