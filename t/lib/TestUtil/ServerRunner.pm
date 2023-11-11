2cf4e031cb06e23b4d090b6ba6d31e543797d4b0
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-12 07:44:29 +0900

    Copy some logic from Test::TCP .

--- t/lib/TestUtil/ServerRunner.pm
+++ t/lib/TestUtil/ServerRunner.pm
@@ -3,6 +3,7 @@ package TestUtil::ServerRunner;
 use strict;
 use warnings;
 use Carp 'confess';
+use Config;
 
 use IO::Socket::INET;
 
@@ -19,7 +20,8 @@ sub new {
   my $class = shift;
   
   my $self = {
-    @_
+    @_,
+    _my_pid => $$,
   };
   
   bless $self, ref $class || $class;
@@ -210,16 +212,43 @@ sub run_echo_server {
 
 # Instance Methods
 sub stop {
-  my ($self) = @_;
-  
-  my $pid = $self->{pid};
-  
-  kill $TERMSIG, $pid;
-  
-  # On Windows, waitpid never return. I don't understan yet this reason(maybe IO blocking).
-  unless ($^O eq 'MSWin32') {
-    waitpid $pid, 0;
-  }
+    my $self = shift;
+ 
+    return unless defined $self->{pid};
+    return unless $self->{_my_pid} == $$;
+ 
+    # This is a workaround for win32 fork emulation's bug.
+    #
+    # kill is inherently unsafe for pseudo-processes in Windows
+    # and the process calling kill(9, $pid) may be destabilized
+    # The call to Sleep will decrease the frequency of this problems
+    #
+    # SEE ALSO:
+    #   http://www.gossamer-threads.com/lists/perl/porters/261805
+    #   https://rt.cpan.org/Ticket/Display.html?id=67292
+    Win32::Sleep(0) if $^O eq "MSWin32"; # will relinquish the remainder of its time slice
+ 
+        kill $TERMSIG => $self->{pid};
+ 
+    Win32::Sleep(0) if $^O eq "MSWin32"; # will relinquish the remainder of its time slice
+ 
+ 
+    local $?; # waitpid modifies original $?.
+    LOOP: while (1) {
+        my $kid = waitpid( $self->{pid}, 0 );
+        if ($^O ne 'MSWin32') { # i'm not in hell
+            if (POSIX::WIFSIGNALED($?)) {
+                my $signame = (split(' ', $Config{sig_name}))[POSIX::WTERMSIG($?)];
+                if ($signame =~ /^(ABRT|PIPE)$/) {
+                    Test::More::diag("your server received SIG$signame");
+                }
+            }
+        }
+        if ($kid == 0 || $kid == -1) {
+            last LOOP;
+        }
+    }
+    undef $self->{pid};
 }
 
 sub DESTROY {
