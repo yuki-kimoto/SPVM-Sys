ff80140f79acede2c34d5b0c252a03dd79de4eca
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-28 13:24:36 +0900

    Revert "Support d_type in Windows."
    This reverts commit be37b0e058c8e4cef29952bc966f626acebd798f.

--- lib/SPVM/Sys/IO/Dirent.c
+++ lib/SPVM/Sys/IO/Dirent.c
@@ -51,6 +51,10 @@ int32_t SPVM__Sys__IO__Dirent__d_reclen(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__Sys__IO__Dirent__d_type(SPVM_ENV* env, SPVM_VALUE* stack) {
+#if defined(_WIN32)
+  env->die(env, stack, "d_type is not supported in this system(defined(_WIN32)).", __func__, FILE_NAME, __LINE__);
+  return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_NOT_SUPPORTED_CLASS;
+#else
   void* obj_dirent = stack[0].oval;
   
   struct dirent* st_dirent = env->get_pointer(env, stack, obj_dirent);
@@ -58,6 +62,7 @@ int32_t SPVM__Sys__IO__Dirent__d_type(SPVM_ENV* env, SPVM_VALUE* stack) {
   stack[0].ival = st_dirent->d_type;
   
   return 0;
+#endif
 }
 
 int32_t SPVM__Sys__IO__Dirent__d_off(SPVM_ENV* env, SPVM_VALUE* stack) {
