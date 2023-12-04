376a2556a37788b7168566b0f9145478b3a45d9d
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-04 09:09:21 +0900

    Reorder headers.

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -3,19 +3,20 @@
 
 #include "spvm_native.h"
 
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <fcntl.h>
+
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
 #include <dirent.h>
-
+#include <fcntl.h>
 #if defined(_WIN32)
   #include <direct.h>
 #endif
+
+#include <errno.h>
+#include <stdlib.h>
 
 const char* FILE_NAME = "Sys/IO.c";
 
