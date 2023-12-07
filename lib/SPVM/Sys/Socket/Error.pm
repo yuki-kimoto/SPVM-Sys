125a8822ea3e673593a032c62416adc843f94b33
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-07 10:57:04 +0900

    Sys::Socket::Error extends Error::System. and etc.   [New Features]     * Sys::Socket::Error extends Error::System.     * Sys::Socket::Error::InetInvalidNetworkAddress extends Sys::Socket::Error.   [Document Fix]     * Add the doc of the Sys::Socket::Error class.

---
+++ lib/SPVM/Sys/Socket/Error.pm
@@ -0,0 +1,28 @@
+package SPVM::Sys::Socket::Error;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Sys::Socket::Error - Socket Errors
+
+=head1 Description
+
+The Sys::Socket::Error class of L<SPVM> represents socket errors.
+
+=head1 Usage
+
+  use Sys::Socket::Error;
+
+=head1 Inheritance
+
+L<Error::System|SPVM::Error::System>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
