4862cb0f8535d592188f90ee2c52dce7c700af82
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-07 13:22:32 +0900

    Improve the doc of Sys::Socket::Sockaddr.

--- lib/SPVM/Sys/Socket/Sockaddr.pm
+++ lib/SPVM/Sys/Socket/Sockaddr.pm
@@ -6,15 +6,17 @@ package SPVM::Sys::Socket::Sockaddr;
 
 SPVM::Sys::Socket::Sockaddr - struct sockaddr in the C language
 
+=head1 Description
+
+The Sys::Socket::Sockaddr class in L<SPVM> represents L<struct sockaddr|https://linux.die.net/man/7/ip> in the C language.
+
 =head1 Usage
 
   use Sys::Socket::Sockaddr;
-  
-=head1 Description
 
-C<Sys::Socket::Sockaddr> is the class for the C<struct sockaddr> in the C language.
+=head1 Details
 
-This is a L<pointer class|SPVM::Document::Language/"Pointer Class">.
+This class is a pointer class. The pointer the instance has is set to a L<struct sockaddr|https://linux.die.net/man/7/ip> object.
 
 =head1 Interfaces
 
@@ -28,9 +30,9 @@ This is a L<pointer class|SPVM::Document::Language/"Pointer Class">.
 
 =head2 new
 
-C<static method new : L<Sys::Socket::Sockaddr::In|SPVM::Sys::Socket::Sockaddr::In> ();>
+C<static method new : L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> ();>
 
-Create a new C<Sys::Socket::Sockaddr> object.
+Create a new L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> object.
 
 =head1 Instance Methods
 
@@ -60,18 +62,18 @@ Returns the size of the structure. This method is planed to be implemented in a 
 
 Exception:
 
-  Not implemented.
+Not implemented.
   
 =head2 clone
 
 C<method clone : L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> ();>
 
-Clones this object. This method is planed to be implemented in a child class.
+Clones this instance. This method is planed to be implemented in a child class.
 
 Exception:
 
-  Not implemented.
-  
+Not implemented.
+
 =head1 Well Known Child Classes
 
 =over 2
@@ -85,14 +87,6 @@ Exception:
 =item L<Sys::Socket::Sockaddr::Storage|SPVM::Sys::Socket::Sockaddr::Storage>
 
 =back
-
-=head1 Copyright & License
-
-Copyright 2022-2022 Yuki Kimoto, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
 
 =head1 Copyright & License
 
