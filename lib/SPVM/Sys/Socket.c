6fbf7c327db2ad97bbc247567ad983b30621d630
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-08 10:04:02 +0900

    Move Errno methods.

--- lib/SPVM/Sys/Socket.c
+++ lib/SPVM/Sys/Socket.c
@@ -17,27 +17,6 @@
 
 static const char* FILE_NAME = "Sys/Socket.c";
 
-int32_t SPVM__Sys__Socket__socket_errno(SPVM_ENV* env, SPVM_VALUE* stack) {
-  int32_t ret_socket_errno = spvm_socket_errno();
-  
-  stack[0].ival = ret_socket_errno;
-  
-  return 0;
-}
-
-int32_t SPVM__Sys__Socket__socket_strerror(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
-  int32_t error_number = stack[0].ival;
-  
-  int32_t length = stack[1].ival;
-  
-  void* obj_socket_strerror = spvm_socket_strerror_string(env, stack, error_number, length);
-  
-  stack[0].oval = obj_socket_strerror;
-  
-  return 0;
-}
-
 int32_t SPVM__Sys__Socket__htonl(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t hostlong = stack[0].ival;
