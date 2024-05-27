1afc8928aa019b54b62210565d22c8e466a58f8f
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-05-27 09:00:10 +0900

    Add empty add, remove, extend methods to Sys::Poll::PollfdArray class.

--- lib/SPVM/Sys/Poll/PollfdArray.c
+++ lib/SPVM/Sys/Poll/PollfdArray.c
@@ -74,7 +74,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__fd(SPVM_ENV* env, SPVM_VALUE* stack) {
   if (!(index < length)) {
     return env->die(env, stack, "The index $index must be less than the value of the length field.", __func__, FILE_NAME, __LINE__);
   }
-
+  
   struct pollfd* fds = env->get_pointer(env, stack, obj_self);
   
   int32_t fd = fds[index].fd;
@@ -89,7 +89,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__set_fd(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t error_id = 0;
   
   void* obj_self = stack[0].oval;
-
+  
   int32_t index = stack[1].ival;
   
   int32_t length = env->get_field_int_by_name(env, stack, obj_self, "length", &error_id, __func__, FILE_NAME, __LINE__);
@@ -102,7 +102,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__set_fd(SPVM_ENV* env, SPVM_VALUE* stack) {
   if (!(index < length)) {
     return env->die(env, stack, "The index $index must be less than the value of the length field.", __func__, FILE_NAME, __LINE__);
   }
-
+  
   int32_t fd = stack[2].ival;
   
   struct pollfd* fds = env->get_pointer(env, stack, obj_self);
@@ -129,7 +129,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__events(SPVM_ENV* env, SPVM_VALUE* stack) {
   if (!(index < length)) {
     return env->die(env, stack, "The index $index must be less than the value of the length field.", __func__, FILE_NAME, __LINE__);
   }
-
+  
   struct pollfd* fds = env->get_pointer(env, stack, obj_self);
   
   int16_t events = fds[index].events;
@@ -144,7 +144,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__set_events(SPVM_ENV* env, SPVM_VALUE* stac
   int32_t error_id = 0;
   
   void* obj_self = stack[0].oval;
-
+  
   int32_t index = stack[1].ival;
   if (!(index >= 0)) {
     return env->die(env, stack, "The index $index must be greater than or equal to 0.", __func__, FILE_NAME, __LINE__);
@@ -156,7 +156,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__set_events(SPVM_ENV* env, SPVM_VALUE* stac
   if (!(index < length)) {
     return env->die(env, stack, "The index $index must be less than the value of the length field.", __func__, FILE_NAME, __LINE__);
   }
-
+  
   int16_t events = stack[2].ival;
   
   struct pollfd* fds = env->get_pointer(env, stack, obj_self);
@@ -182,7 +182,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__revents(SPVM_ENV* env, SPVM_VALUE* stack) 
   if (!(index < length)) {
     return env->die(env, stack, "The index $index must be less than the value of the length field.", __func__, FILE_NAME, __LINE__);
   }
-
+  
   struct pollfd* fds = env->get_pointer(env, stack, obj_self);
   
   int16_t revents = fds[index].revents;
@@ -197,7 +197,7 @@ int32_t SPVM__Sys__Poll__PollfdArray__set_revents(SPVM_ENV* env, SPVM_VALUE* sta
   int32_t error_id = 0;
   
   void* obj_self = stack[0].oval;
-
+  
   int32_t index = stack[1].ival;
   if (!(index >= 0)) {
     return env->die(env, stack, "The index $index must be greater than or equal to 0.", __func__, FILE_NAME, __LINE__);
@@ -209,12 +209,45 @@ int32_t SPVM__Sys__Poll__PollfdArray__set_revents(SPVM_ENV* env, SPVM_VALUE* sta
   if (!(index < length)) {
     return env->die(env, stack, "The index $index must be less than the value of the length field.", __func__, FILE_NAME, __LINE__);
   }
-
+  
   int16_t revents = stack[2].ival;
   
   struct pollfd* fds = env->get_pointer(env, stack, obj_self);
   
   fds[index].revents = revents;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Poll__PollfdArray__add(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  int32_t fd = stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Poll__PollfdArray__remove(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  int32_t fd = stack[1].ival;
+  
+  return 0;
+}
+
+int32_t SPVM__Sys__Poll__PollfdArray___maybe_extend(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  int32_t capacity = stack[1].ival;
   
   return 0;
 }
