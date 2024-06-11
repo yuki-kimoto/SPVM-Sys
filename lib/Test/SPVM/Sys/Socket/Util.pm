bba5aaf4c33021b74c686ad9d7907372a72e841c
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-06-11 09:26:13 +0900

    Add run_echo_server to Test::SPVM::Sys::Socket::Util.

--- lib/Test/SPVM/Sys/Socket/Util.pm
+++ lib/Test/SPVM/Sys/Socket/Util.pm
@@ -6,6 +6,32 @@ use Carp ();
 
 use Socket;
 
+my $localhost = "127.0.0.1";
+
+# Starts a echo server
+sub run_echo_server {
+  my ($class, $port) = @_;
+  
+  my $server_socket = IO::Socket::INET->new(
+    LocalAddr => $localhost,
+    LocalPort => $port,
+    Listen    => SOMAXCONN,
+    Proto     => 'tcp',
+    Reuse => 1,
+  );
+  unless ($server_socket) {
+    Carp::confess("Can't create a server socket:$@");
+  }
+  
+  while (1) {
+    my $client_socket = $server_socket->accept;
+    
+    while (my $data = <$client_socket>) {
+      print $client_socket $data;
+    }
+  }
+}
+
 1;
 
 =head1 Name
