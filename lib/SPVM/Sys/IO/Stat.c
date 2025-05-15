917de0f278732941ddfb5bdb367b3138e72ea59c
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-15 10:38:59 +0900

    Implement S_follow_symlinks_to.

--- lib/SPVM/Sys/IO/Stat.c
+++ lib/SPVM/Sys/IO/Stat.c
@@ -384,6 +384,30 @@ typedef uint32_t STRLEN;
 #define MKTIME_UNLOCK
 #define Zero(ptr, size, type) memset(ptr, 0, size * sizeof(type));
 typedef uint64_t Off_t;
+#define PERL_ARGS_ASSERT_MY_MEMRCHR
+
+// Exactly same as Perl's one in inline.h
+void *
+S_my_memrchr(const char * s, const char c, const STRLEN len)
+{
+    /* memrchr(), since many platforms lack it */
+
+    const char * t = s + len - 1;
+
+    PERL_ARGS_ASSERT_MY_MEMRCHR;
+
+    while (t >= s) {
+        if (*t == c) {
+            return (void *) t;
+        }
+        t--;
+    }
+
+    return NULL;
+}
+
+// Exactly same as Perl's one in embed.h
+#define my_memrchr S_my_memrchr
 
 // Exactly same as Perl's one in Win32.h
 #define _S_IFLNK ((unsigned)(_S_IFDIR | _S_IFCHR))
@@ -418,47 +442,7 @@ struct w32_stat {
 #endif
 
 // Exactly same as Perl's one in Win32.c
-static void
-translate_to_errno(void)
-{
-    /* This isn't perfect, eg. Win32 returns ERROR_ACCESS_DENIED for
-       both permissions errors and if the source is a directory, while
-       POSIX wants EACCES and EPERM respectively.
-    */
-    switch (GetLastError()) {
-    case ERROR_BAD_NET_NAME:
-    case ERROR_BAD_NETPATH:
-    case ERROR_BAD_PATHNAME:
-    case ERROR_FILE_NOT_FOUND:
-    case ERROR_FILENAME_EXCED_RANGE:
-    case ERROR_INVALID_DRIVE:
-    case ERROR_PATH_NOT_FOUND:
-      errno = ENOENT;
-      break;
-    case ERROR_ALREADY_EXISTS:
-      errno = EEXIST;
-      break;
-    case ERROR_ACCESS_DENIED:
-      errno = EACCES;
-      break;
-    case ERROR_PRIVILEGE_NOT_HELD:
-      errno = EPERM;
-      break;
-    case ERROR_NOT_SAME_DEVICE:
-      errno = EXDEV;
-      break;
-    case ERROR_DISK_FULL:
-      errno = ENOSPC;
-      break;
-    case ERROR_NOT_ENOUGH_QUOTA:
-      errno = EDQUOT;
-      break;
-    default:
-      /* ERROR_INVALID_FUNCTION - eg. symlink on a FAT volume */
-      errno = EINVAL;
-      break;
-    }
-}
+#define SYMLINK_FOLLOW_LIMIT 63
 
 // Exactly same as Perl's one in Win32.c
 #ifndef IO_REPARSE_TAG_SYMLINK
@@ -523,11 +507,47 @@ typedef struct {
 
 __thread SPVM_ENV* thread_env;
 
-static HANDLE
-S_follow_symlinks_to(const char *pathname, DWORD *reparse_type) {
-  
-  // TODO
-  assert(0);
+// Exactly same as Perl's one in Win32.c
+static void
+translate_to_errno(void)
+{
+    /* This isn't perfect, eg. Win32 returns ERROR_ACCESS_DENIED for
+       both permissions errors and if the source is a directory, while
+       POSIX wants EACCES and EPERM respectively.
+    */
+    switch (GetLastError()) {
+    case ERROR_BAD_NET_NAME:
+    case ERROR_BAD_NETPATH:
+    case ERROR_BAD_PATHNAME:
+    case ERROR_FILE_NOT_FOUND:
+    case ERROR_FILENAME_EXCED_RANGE:
+    case ERROR_INVALID_DRIVE:
+    case ERROR_PATH_NOT_FOUND:
+      errno = ENOENT;
+      break;
+    case ERROR_ALREADY_EXISTS:
+      errno = EEXIST;
+      break;
+    case ERROR_ACCESS_DENIED:
+      errno = EACCES;
+      break;
+    case ERROR_PRIVILEGE_NOT_HELD:
+      errno = EPERM;
+      break;
+    case ERROR_NOT_SAME_DEVICE:
+      errno = EXDEV;
+      break;
+    case ERROR_DISK_FULL:
+      errno = ENOSPC;
+      break;
+    case ERROR_NOT_ENOUGH_QUOTA:
+      errno = EDQUOT;
+      break;
+    default:
+      /* ERROR_INVALID_FUNCTION - eg. symlink on a FAT volume */
+      errno = EINVAL;
+      break;
+    }
 }
 
 // Exactly same as Perl's one in Win32.c
@@ -640,6 +660,115 @@ win32_readlink(const char *pathname, char *buf, size_t bufsiz) {
     }
 
     return bytes_out;
+}
+
+// The logic is the same as Perl's one in Win32.c, but this function use Perl data structure SV. I replace it with SPVM data structure.
+static HANDLE
+S_follow_symlinks_to(const char *pathname, DWORD *reparse_type) {
+    
+    SPVM_ENV* env = thread_env;
+    
+    SPVM_VALUE* stack = env->new_stack(env);
+    
+    int32_t scope_id = env->enter_scope(env, stack);
+    
+    char link_target[MAX_PATH];
+    void* work_path = env->new_string(env, stack, pathname, strlen(pathname));
+    int link_count = 0;
+    int link_len;
+    HANDLE handle;
+    
+    *reparse_type = 0;
+    
+    while ((link_len = win32_readlink(env->get_chars(env, stack, work_path), link_target,
+                                      sizeof(link_target))) > 0) {
+        if (link_count++ >= SYMLINK_FOLLOW_LIMIT) {
+            /* Windows doesn't appear to ever return ELOOP,
+               let's do better ourselves
+            */
+            errno = ELOOP;
+            handle = INVALID_HANDLE_VALUE;
+            goto END_OF_FUNC;
+        }
+        /* Adjust the linktarget based on the link source or current
+           directory as needed.
+        */
+        if (link_target[0] == '\\'
+            || link_target[0] == '/'
+            || (link_len >=2 && link_target[1] == ':')) {
+            /* link is absolute */
+            work_path = env->new_string(env, stack, link_target, link_len);
+        }
+        else {
+            STRLEN work_len;
+            const char *workp = env->get_chars(env, stack, work_path);
+            const char *final_bslash =
+                (const char *)my_memrchr(workp, '\\', work_len);
+            const char *final_slash =
+                (const char *)my_memrchr(workp, '/', work_len);
+            const char *path_sep = NULL;
+            if (final_bslash && final_slash)
+                path_sep = final_bslash > final_slash ? final_bslash : final_slash;
+            else if (final_bslash)
+                path_sep = final_bslash;
+            else if (final_slash)
+                path_sep = final_slash;
+
+            if (path_sep) {
+                void* new_path = env->new_string(env, stack, workp, path_sep - workp + 1);
+                env->concat(env, stack, new_path, env->new_string(env, stack, link_target, link_len));
+                work_path = new_path;
+            }
+            else {
+                /* should only get here the first time around */
+                assert(link_count == 1);
+                char path_temp[MAX_PATH];
+                DWORD path_len = GetCurrentDirectoryA(sizeof(path_temp), path_temp);
+                if (!path_len || path_len > sizeof(path_temp)) {
+                    errno = EINVAL;
+                    handle = INVALID_HANDLE_VALUE;
+                    goto END_OF_FUNC;
+                }
+
+                void* new_path = env->new_string(env, stack, path_temp, path_len);
+                if (path_temp[path_len-1] != '\\') {
+                    new_path = env->concat(env, stack, new_path, env->new_string_nolen(env, stack, "\\"));
+                }
+                new_path = env->concat(env, stack, new_path, env->new_string(env, stack, link_target, link_len));
+                work_path = new_path;
+            }
+        }
+    }
+
+    handle =
+        CreateFileA(env->get_chars(env, stack, work_path), GENERIC_READ, 0, NULL, OPEN_EXISTING,
+                    FILE_FLAG_OPEN_REPARSE_POINT|FILE_FLAG_BACKUP_SEMANTICS, 0);
+    if (handle != INVALID_HANDLE_VALUE) {
+        MY_REPARSE_DATA_BUFFER linkdata;
+        DWORD linkdata_returned;
+
+        if (!DeviceIoControl(handle, FSCTL_GET_REPARSE_POINT, NULL, 0,
+                             &linkdata, sizeof(linkdata),
+                             &linkdata_returned, NULL)) {
+            translate_to_errno();
+            CloseHandle(handle);
+            handle = INVALID_HANDLE_VALUE;
+            goto END_OF_FUNC;
+        }
+        *reparse_type = linkdata.ReparseTag;
+        goto END_OF_FUNC;
+    }
+    else {
+        translate_to_errno();
+    }
+    
+    END_OF_FUNC:
+    
+    env->leave_scope(env, stack, scope_id);
+    
+    env->free_stack(env, stack);
+    
+    return handle;
 }
 
 // Exactly same as Perl's one in Win32.c
