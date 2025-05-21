56944ed990c84d332e5b9aef3244d46323d43363
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-21 10:32:27 +0900

    Add utf16le_to_utf8.

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -67,11 +67,52 @@ static void* utf8_to_utf16le(SPVM_ENV* env, SPVM_VALUE* stack, void* obj_utf8_st
     return NULL;
   }
   
-  return utf16le_string;
+  return obj_utf16le_string;
 }
 
-static void* utf16le_to_utf8(SPVM_ENV* env, SPVM_VALUE* stack, void* utf16u_string) {
+static void* utf16le_to_utf8(SPVM_ENV* env, SPVM_VALUE* stack, void* utf16le_string, int32_t* error_id, const char* func_name, const char* file, int32_t line) {
   
+  if (utf16le_string == NULL) {
+    return NULL;
+  }
+  
+  int32_t utf8_string_length = WideCharToMultiByte(
+    CP_UTF8,
+    MB_ERR_INVALID_CHARS,
+    utf16le_string,
+    -1,
+    NULL,
+    0,
+    NULL,
+    NULL
+  );
+  
+  if (utf8_string_length == 0) {
+    *error_id = env->die(env, stack,  "utf8_to_utf16le failed:Error calculating length: %lu.", GetLastError(), func_name, file, line);
+    return NULL;
+  }
+  
+  void* obj_utf8_string = env->new_string(env, stack, NULL, utf8_string_length);
+  
+  char* utf8_string = (char*)env->get_chars(env, stack, obj_utf8_string);
+  
+  utf8_string_length = WideCharToMultiByte(
+    CP_UTF8,
+    MB_ERR_INVALID_CHARS,
+    utf16le_string,
+    -1,
+    utf8_string,
+    utf8_string_length,
+    NULL,
+    NULL
+  );
+  
+  if (utf8_string_length == 0) {
+    *error_id = env->die(env, stack,  "utf16le_to_utf8 failed:Error converting UTF-16LE to UTF-8: %lu.", GetLastError(), func_name, file, line);
+    return NULL;
+  }
+  
+  return obj_utf8_string;
 }
 
 #endif // defined(_WIN32)
