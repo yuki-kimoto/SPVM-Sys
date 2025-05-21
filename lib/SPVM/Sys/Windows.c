681b68365cb0399e092f97f6c177fc8c67e4ee99
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-21 15:12:12 +0900

    Move utf8_to_utf16le to Sys/Windows.native/include/Sys-Windows.h

---
+++ lib/SPVM/Sys/Windows.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Sys/Windows.c";
+
+int32_t SPVM__Sys__Windows__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
