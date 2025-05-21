350b1082eaadc4a6b9e757e4b78711c1c24d38c2
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-22 08:13:22 +0900

    Sys::IO#rmdir support UTF-8 in Windows.

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -934,6 +934,8 @@ int32_t SPVM__Sys__IO__umask(SPVM_ENV* env, SPVM_VALUE* stack) {
 
 int32_t SPVM__Sys__IO__rmdir(SPVM_ENV* env, SPVM_VALUE* stack) {
   
+  int32_t error_id = 0;
+  
   void* obj_path = stack[0].oval;
   
   if (!obj_path) {
@@ -942,7 +944,17 @@ int32_t SPVM__Sys__IO__rmdir(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   const char* path = env->get_chars(env, stack, obj_path);
   
+#if defined(_WIN32)
+  wchar_t* path_w = utf8_to_win_wchar(env, stack, path, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) {
+    return error_id;
+  }
+  
+  int32_t status = _wrmdir(path_w);
+#else
   int32_t status = rmdir(path);
+#endif
+
   if (status == -1) {
     env->die(env, stack, "[System Error]rmdir() failed:%s. $path is \"%s\".", env->strerror_nolen(env, stack, errno), path, __func__, FILE_NAME, __LINE__);
     return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
