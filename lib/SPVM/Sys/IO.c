0e76009bfb5395b35e92f0e2878709ebf55d0c45
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-22 08:20:19 +0900

    Sys::IO#chdir support UTF-8 in Windows.

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -1186,13 +1186,25 @@ int32_t SPVM__Sys__IO___fullpath(SPVM_ENV* env, SPVM_VALUE* stack) {
 
 int32_t SPVM__Sys__IO__chdir(SPVM_ENV* env, SPVM_VALUE* stack) {
   
+  int32_t error_id = 0;
+  
   void* obj_path = stack[0].oval;
   if (!obj_path) {
     return env->die(env, stack, "The path $path must be defined.", __func__, FILE_NAME, __LINE__);
   }
   const char* path = env->get_chars(env, stack, obj_path);
   
+#if defined(_WIN32)
+  wchar_t* path_w = utf8_to_win_wchar(env, stack, path, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) {
+    return error_id;
+  }
+  
+  int32_t status = _wchdir(path_w);
+#else
   int32_t status = chdir(path);
+#endif
+
   if (status == -1) {
     env->die(env, stack, "[System Error]chdir() failed:%s. $path is \"%s\".", env->strerror_nolen(env, stack, errno), path, __func__, FILE_NAME, __LINE__);
     return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
