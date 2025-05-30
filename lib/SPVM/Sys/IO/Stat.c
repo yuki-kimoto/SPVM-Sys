766c4e7b7481581acbae2dacad7fe3a352e2855c
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-30 11:41:09 +0900

    Refactor win_fstat_by_handle.

--- lib/SPVM/Sys/IO/Stat.c
+++ lib/SPVM/Sys/IO/Stat.c
@@ -165,29 +165,36 @@ static int32_t win_fstat_by_handle(SPVM_ENV* env, SPVM_VALUE* stack, HANDLE hand
             }
           }
           else {
-            WCHAR path_tmp_w[MAX_PATH+1];
             st_stat->st_mode = _S_IFREG;
             
-            const char* path = NULL;
-            int32_t len = GetFinalPathNameByHandleW(handle, path_tmp_w, sizeof(path_tmp_w), 0);
-            if (len > 0) {
-              int32_t scope_id = env->enter_scope(env, stack);
-              
-              WCHAR* path_w = env->new_memory_block(env, stack, sizeof(WCHAR) * (len + 1));
-              
-              len = GetFinalPathNameByHandleW(handle, path_w, len + 1, 0);
-              
-              assert(len > 0);
-              
-              int32_t error_id = 0;
-              
-              path = spvm_sys_windows_win_wchar_to_utf8(env, stack, path_w, &error_id, __func__, FILE_NAME, __LINE__);
-              
-              env->free_memory_block(env, stack, path_w);
-              
-              assert(error_id == 0);
-              
-              env->leave_scope(env, stack, scope_id);
+            int32_t needed_len = GetFinalPathNameByHandleW(handle, NULL, 0, 0);
+            
+            if (needed_len == 0) {
+              spvm_sys_windows_win_last_error_to_errno(EINVAL);
+              goto END_OF_FUNC;
+            }
+            
+            int32_t scope_id = env->enter_scope(env, stack);
+            
+            WCHAR* path_w = env->new_memory_block(env, stack, sizeof(WCHAR) * (needed_len + 1));
+            
+            int32_t len = GetFinalPathNameByHandleW(handle, path_w, needed_len + 1, 0);
+            
+            if (len == 0) {
+              spvm_sys_windows_win_last_error_to_errno(EINVAL);
+              goto END_OF_FUNC;
+            }
+            
+            int32_t error_id = 0;
+            
+            const char* path = spvm_sys_windows_win_wchar_to_utf8(env, stack, path_w, &error_id, __func__, FILE_NAME, __LINE__);
+            
+            env->free_memory_block(env, stack, path_w);
+            
+            env->leave_scope(env, stack, scope_id);
+            
+            if (error_id) {
+              goto END_OF_FUNC;
             }
             
             if (path && len > 4 &&
