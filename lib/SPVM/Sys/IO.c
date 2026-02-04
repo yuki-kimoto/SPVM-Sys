44464ba175ea3bb45e3a1c13d8cf6638b9a1089d
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-04 14:21:23 +0900

    Add missing format specifier for pathname and sync arguments in env->die call within SPVM__Sys__IO__access

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -828,7 +828,7 @@ int32_t SPVM__Sys__IO__access(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   if (status == -1) {
     const char* pathname = env->get_chars(env, stack, obj_pathname);
-    env->die(env, stack, "[System Error]access() failed(%d: %s).", __func__, FILE_NAME, __LINE__, errno, env->strerror_nolen(env, stack, errno), pathname);
+    env->die(env, stack, "[System Error]access() failed(%d: %s). $pathname='%s'.", __func__, FILE_NAME, __LINE__, errno, env->strerror_nolen(env, stack, errno), pathname);
     return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
   }
   
