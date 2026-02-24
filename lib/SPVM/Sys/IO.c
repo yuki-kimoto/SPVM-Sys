6e5aa00bf45206801e4c0dbeb5204eb63563f5e8
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-24 13:28:17 +0900

    Define _XOPEN_SOURCE and _DEFAULT_SOURCE to fix Linux build errors.

--- lib/SPVM/Sys/IO.c
+++ lib/SPVM/Sys/IO.c
@@ -1,8 +1,11 @@
 // Copyright (c) 2023 Yuki Kimoto
 // MIT License
 
-/* Enable X/Open System Interfaces (SUSv4) functions and POSIX.1-2008 standard functions */
+// Enable X/Open System Interfaces (SUSv4) functions and POSIX.1-2008 standard functions on Linux and macOS
 #define _XOPEN_SOURCE 700
+
+// Enable BSD and System V extensions on Linux
+#define _DEFAULT_SOURCE
 
 #include "spvm_native.h"
 
