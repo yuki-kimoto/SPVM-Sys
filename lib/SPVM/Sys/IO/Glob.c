4be19e669ff0ef9cb6ea9320bd2a2ece2a3f8433
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-05-07 08:27:16 +0900

    Refactor SPVM__Sys__IO__Glob__bsd_glob

--- lib/SPVM/Sys/IO/Glob.c
+++ lib/SPVM/Sys/IO/Glob.c
@@ -15,53 +15,48 @@ static const char* FILE_NAME = "Sys/IO/Glob.c";
 
 int32_t SPVM__Sys__IO__Glob__bsd_glob(SPVM_ENV* env, SPVM_VALUE* stack) {
   
+  int32_t error_id = 0;
+  
   void* obj_pattern = stack[0].oval;
   if (!obj_pattern) {
-    return env->die(env, stack, "The $pattern must be defined", __func__, FILE_NAME, __LINE__);
+    return env->die(env, stack, "Pattern $pattern must be defined.", __func__, FILE_NAME, __LINE__);
   }
-  char* pattern = (char*)env->get_chars(env, stack, obj_pattern);
+  const char* pattern = env->get_chars(env, stack, obj_pattern);
   
-  if (pattern[0] == '~') {
-    return env->die(env, stack, "The \"~\" expansion is not supported", __func__, FILE_NAME, __LINE__);
-  }
-  
-  glob_t pglob;
   int32_t flags = 0;
-  void* errfunc = NULL;
-  
   flags |= GLOB_BRACE | GLOB_NOMAGIC | GLOB_QUOTE | GLOB_TILDE | GLOB_ALPHASORT;
-  
 #ifdef WIN32
   flags |= GLOB_NOCASE;
 #endif
   
-  memset(&pglob, 0, sizeof(glob_t));
+  void* errfunc = NULL;
+  
+  glob_t pglob = {0};
   
   int32_t status = bsd_glob(pattern, flags, errfunc, &pglob);
   
-  int32_t e = 0;
   if (!(status == 0)) {
-    env->die(env, stack, "[System Error]glob failed:%s. The files specified by the \"%s\" pattern can't be get", __func__, FILE_NAME, __LINE__, env->strerror(env, stack, errno, 0), pattern);
-    e = SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
+    env->die(env, stack, "[System Error]bsd_glob failed:(%d:%s). $pattern='%s'.", __func__, FILE_NAME, __LINE__, errno, env->strerror(env, stack, errno, 0), pattern);
+    error_id = SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_SYSTEM_CLASS;
+    goto END_OF_FUNC;
   }
-  else {
-    int32_t paths_length = pglob.gl_pathc;
-    
-    void* obj_paths = env->new_string_array(env, stack, paths_length);
-    
-    for (int i = 0; i < pglob.gl_pathc; i++) {
-      const char* path = pglob.gl_pathv[i];
-      int32_t path_length = strlen(path);
-      void* obj_path = env->new_string(env, stack, path, path_length);
-      env->set_elem_object(env, stack, obj_paths, i, obj_path);
-    }
-    
-    stack[0].oval = obj_paths;
+  
+  int32_t paths_length = pglob.gl_pathc;
+  void* obj_paths = env->new_string_array(env, stack, paths_length);
+  for (int i = 0; i < pglob.gl_pathc; i++) {
+    const char* path = pglob.gl_pathv[i];
+    int32_t path_length = strlen(path);
+    void* obj_path = env->new_string(env, stack, path, path_length);
+    env->set_elem_object(env, stack, obj_paths, i, obj_path);
   }
+  
+  stack[0].oval = obj_paths;
+  
+  END_OF_FUNC:
   
   bsd_globfree(&pglob);
   
-  return e;
+  return error_id;
 }
 
 #if 0
