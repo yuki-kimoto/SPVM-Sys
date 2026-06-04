3aed37cc15e4e062f82ff51373fd3af98ded5a5c
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-06-04 09:38:54 +0900

    Fix  d3ffdd9e1a7c23d862f14e8cdd5c09b00631652c .

--- lib/SPVM/Sys/Time.c
+++ lib/SPVM/Sys/Time.c
@@ -203,7 +203,7 @@ int32_t SPVM__Sys__Time__clock_getres(SPVM_ENV* env, SPVM_VALUE* stack) {
   }
   
 #ifdef _WIN32
-  int32_t status = sys_windows_clock_getres(clk_id, st_res);
+  int32_t status = spvm_sys_windows_clock_getres(clk_id, st_res);
 #else
   int32_t status = clock_getres(clk_id, st_res);
 #endif
